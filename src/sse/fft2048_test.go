4a19262bb5650a44dd93070907811b9215afff54
a4a881d4 <a4a881d4@163.com>
2015-09-30 00:36:01 +0800

    testing

--- src/sse/fft2048_test.go
+++ src/sse/fft2048_test.go
@@ -9,7 +9,8 @@ import "cmplxv"
 //import "fmt"
 
 func TestFFT2048(t *testing.T) {
-	aSin := cmplxv.Arange(2048).Mul(0. / 2048.).Cmul(complex(0., math.Pi*2)).Exp().Mul(64.)
+	k := 1024
+	aSin := cmplxv.Arange(2048).Mul(float64(-k) / 2048.).Cmul(complex(0., math.Pi*2)).Exp().Mul(64.)
 	in := ToM128Buf(*aSin)
 	fplan := NewFreqCorrectPlan()
 	plan := NewFFT2048Plan()
@@ -17,18 +18,21 @@ func TestFFT2048(t *testing.T) {
 	fSin := NewCmplx32Vec(2048)
 	ifSin := NewCmplx32Vec(2048)
 
-	for i := 0; i < 2048; i++ {
+	for i := k; i < k+128; i++ {
 		iplan.Do(in, fSin)
+		//fmt.Println("fSin", fSin.v[:5])
 		fplan.DoA(fSin, fSin, 0, 0, 1024)
+		//fmt.Println("fSin", fSin.v[:5])
 		plan.Do(fSin, ifSin)
 		rSin := ifSin.FromBuf()
 		err := rSin.Vsub(in.FromBuf().Mul(1.))
 		fplan.Do(in, in, 0, 4*65536)
 		avg := math.Sqrt(err.Power() / 2048.)
-		if avg > 8. {
+		if avg > 16. {
 			inx, peak := err.FindMax()
 			finx, fpeak, _ := fSin.FindMax()
-			t.Error(i, finx, fpeak, inx, peak, (*err)[inx], in.v[inx], fSin.v[i], ifSin.v[inx])
+			t.Error(i, finx, fpeak, inx, peak, (*err)[inx], in.v[inx], fSin.v[i], ifSin.v[inx], avg)
+			t.Error(in.v[:5], ifSin.v[:5])
 
 		}
 	}
